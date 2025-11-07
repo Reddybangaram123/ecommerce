b8c5db2d3623971d99576ff9e5af9cbb1826f896
raavi yamini <143521631+yaminiraavi@users.noreply.github.com>
2025-11-07 12:27:29 +0530

    added Backend API to Fashion Category

--- Server/server.js
+++ Server/server.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const bookrouter = require("./routes/bookrouter");
+const Fashionrouter = require("./routes/Fashionrouter");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // ✅ MongoDB connection
-mongoose.connect("mongodb://127.0.0.1:27017/Books")
+mongoose.connect("mongodb://127.0.0.1:27017/Fashion")
   .then(() => console.log("✅ MongoDB connected successfully..."))
   .catch(err => console.log("❌ MongoDB connection error:", err));
 
 // ✅ Use Routes
-app.use("/", bookrouter);
+app.use("/", Fashionrouter);
 // ✅ Start server
 app.listen(3001, () => {
   console.log("🚀 Server running on port 3001...");
